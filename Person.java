2bf56ba0bc5563f0892a897723e5cdb3703d61b6
RoshiniR <r.roshini.11@gmail.com>
2018-06-06 16:53:09 +0530

    Create Person.java
    Creating Base class Person.java Need to add Account for each person

---
+++ Person.java
@@ -0,0 +1,19 @@
+
+
+public class Person
+{
+    public String name;
+
+    Person(){
+    }
+    
+    public void putName(String name)
+    {
+        this.name=name;
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }
+}
